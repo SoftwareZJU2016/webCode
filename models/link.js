7f3617361d960194c0e73d532771271aa9414c0a
870247639 <870247639@qq.com>
2016-12-19 23:37:03 +0800

    link.js
    models/link.js 友情链接 finish
    topbar.pug 增加退出按钮

--- models/link.js
+++ models/link.js
@@ -2,6 +2,7 @@ var pool = require('./index');
 
 var Link = {};
 
+/*得到一门课的所有友情链接*/
 Link.get = (courseID, callback) => {
     pool.getConnection((err, connection) => {
         if (err) {
@@ -9,15 +10,111 @@ Link.get = (courseID, callback) => {
             return;
         }
         var query = 'SELECT * FROM link WHERE course_id = ?';
-        connection.query(query, [courseID], (err, results, fields) => {
-            if (err) { 
+    connection.query(query, [courseID], (err, results, fields) => {
+        if (err) {
+            console.log(err);
+            results = [];
+        }
+        connection.release();
+    callback(results);
+});
+});
+}
+
+/*更新一门课的某个友情链接的内容*/
+Link.updateName = function (courseID, name, url, callback) {
+    pool.getConnection(function (err, connection) {
+        if(err) {
+            console.log(err);
+            return ;
+        }
+        var sql = 'UPDATE link SET content = ? WHERE course_id = ? and url = ?';
+        connection.query(sql, [name, courseID, url], function(err, result, fields){
+            if(err){
                 console.log(err);
-                results = [];
+                return ;
+            }
+            if(result.affectedRows == 0){
+                callback(false);
+            }else{
+                callback(true);
             }
             connection.release();
-            callback(results);
-        });
-    });
+
+
+        })
+    })
 }
+
+/*更新一门课的某个友情链接的url*/
+Link.updateURL = function (courseID, name, url, callback) {
+    pool.getConnection(function (err, connection) {
+        if(err) {
+            console.log(err);
+            return ;
+        }
+        var sql = 'UPDATE link SET url = ? WHERE course_id = ? and content = ?';
+        connection.query(sql, [url, courseID, name], function(err, result, fields){
+            if(err){
+                console.log(err);
+                return ;
+            }
+            if(result.affectedRows == 0){
+                callback(false);
+            }else{
+                callback(true);
+            }
+            connection.release();
+        })
+    })
+}
+
+/*添加一门课的一个友情链接*/
+Link.add = function (courseID, name, url, callback) {
+    pool.getConnection(function (err, connection) {
+        if(err) {
+            console.log(err);
+            return ;
+        }
+        var sql = 'INSERT INTO link VALUES (?, ?, ?)';
+        connection.query(sql, [courseID, name, url], function(err, result, fields){
+            if(err){
+                console.log(err);
+                return ;
+            }
+            if(result.affectedRows == 0){
+                callback(false);
+            }else{
+                callback(true);
+            }
+            connection.release();
+        })
+    })
+}
+
+/*删除一门课的某个友情链接*/
+Link.delete = function (courseID, name, url, callback) {
+    pool.getConnection(function (err, connection) {
+        if(err) {
+            console.log(err);
+            return ;
+        }
+        var sql = 'DELETE FROM link WHERE course_id = ? and url = ? and content = ?';
+        connection.query(sql, [courseID, url, name], function(err, result, fields){
+            if(err){
+                console.log(err);
+                return ;
+            }
+            if(result.affectedRows == 0){
+                callback(false);
+            }else{
+                callback(true);
+            }
+            connection.release();
+        })
+    })
+}
+
+
 
 module.exports = Link;
