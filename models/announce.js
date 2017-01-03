82c6007aaed1dc0e1b8e92e08c35a711ef75af8c
pu yang <pu yang>
2017-01-03 14:23:14 +0800

    修改module中announce.js

--- models/announce.js
+++ models/announce.js
@@ -18,4 +18,37 @@ Announce.getByID = (annoID, callback) => {
     });
 }
 
+Announce.add = (creator_id, title, content, callback) => {
+    pool.getConnection((err, connection) => {
+        if (err) console.log(err);
+
+        var query = 'INSERT INTO announce(creator_id, post_time, title, content) \
+                     VALUES(?, NOW(), ?, ?)';
+        connection.query(query, [creator_id, title, content], (err, results, fields) => {
+            if (err) { 
+                console.log(err);
+                results = null;
+            }
+            callback(results.affectedRows == 0 ? false : true);
+            connection.release();
+        });
+    });
+}
+
+Announce.getRecentRecord = (creator_id, callback) => {
+ pool.getConnection((err, connection) => {
+        if (err) console.log(err);
+
+        var query = 'SELECT title, content, max(post_time) FROM announce WHERE creator_id = ?';
+        connection.query(query, [creator_id], (err, results, fields) => {
+            if (err) { 
+                console.log(err);
+                results[0] = null;
+            }
+            connection.release();
+            callback(results[0]);
+        });
+    });
+}
+
 module.exports = Announce;
